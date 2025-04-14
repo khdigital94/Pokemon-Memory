8566779b48209718cef9c0712ce0c0ab2763b2e8
Kevin Henrich <153145008+khdigital94@users.noreply.github.com>
2025-04-14 02:24:12 +0200

    Adding handleClick function

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,7 @@
-export default function Card({ id, name, sprite }) {
+export default function Card({ id, name, sprite, handleClick }) {
 	const formattedName = name.charAt(0).toUpperCase() + name.slice(1);
 	return (
-		<div key={id} className="flex flex-col justify-center items-center p-4 w-64 max-w-[50%] rounded overflow-hidden text-slate-900 hover:text-slate-100 bg-slate-100 hover:bg-slate-700 ease-in-out transition cursor-pointer">
+		<div key={id} onClick={() => handleClick(id)} className="flex flex-col justify-center items-center p-4 w-64 max-w-[50%] rounded overflow-hidden text-slate-900 hover:text-slate-100 bg-slate-100 hover:bg-slate-700 ease-in-out transition cursor-pointer">
 			<img src={sprite} alt={formattedName} className="w-48" />
 			<h3 className="text-xl font-bold">{formattedName}</h3>
 		</div>
